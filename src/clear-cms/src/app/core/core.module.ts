9ab508f5e0010ad28576c42090566f974b2ba782
Paul Coronia <pa.coronia@accelerategroup.net>
2020-10-19 22:47:57 +0800

    Add core components

---
+++ src/clear-cms/src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, NavigationComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [HeaderComponent, FooterComponent, NavigationComponent]
+})
+export class CoreModule { }
