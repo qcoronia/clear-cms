20f5ff1e7b25115787bc1a6d03625f2d3f51b360
Paul Coronia <pa.coronia@accelerategroup.net>
2020-10-19 16:05:36 +0800

    Rename product to clear-cms due to coincidental name conflict

--- src/clear-cms/src/app/app.component.spec.ts
+++ src/clear-cms/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pure-cms'`, () => {
+  it(`should have as title 'clear-cms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pure-cms');
+    expect(app.title).toEqual('clear-cms');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pure-cms app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('clear-cms app is running!');
   });
 });
