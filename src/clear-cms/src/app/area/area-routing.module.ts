e0ee4714f1df287c6e0e661f60c5eea3443b04ef
Paul Coronia <pa.coronia@accelerategroup.net>
2020-10-20 12:15:50 +0800

    Add content page

--- src/clear-cms/src/app/area/area-routing.module.ts
+++ src/clear-cms/src/app/area/area-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard' },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'content', loadChildren: () => import('./content/content.module').then(m => m.ContentModule) },
 ];
 
 @NgModule({
