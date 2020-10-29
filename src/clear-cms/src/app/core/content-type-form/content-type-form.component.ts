4040b85ac701ec14f5b2ec3df3579a579c6dd932
Paul Coronia <pa.coronia@accelerategroup.net>
2020-10-29 19:57:24 +0800

    Can revert form

--- src/clear-cms/src/app/core/content-type-form/content-type-form.component.ts
+++ src/clear-cms/src/app/core/content-type-form/content-type-form.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ReplaySubject, Subject, Observable } from 'rxjs';
 import { ContentType } from 'src/app/services/database/models/contentType.model';
-import { takeWhile, takeUntil, tap, switchMap, shareReplay } from 'rxjs/operators';
+import { takeWhile, takeUntil, tap, switchMap, shareReplay, pairwise, map, filter } from 'rxjs/operators';
 import { FormBuilder, FormGroup, FormArray, AbstractControl } from '@angular/forms';
 import { ContentTypeService } from 'src/app/services/content-type/content-type.service';
 import { DataTypeService } from 'src/app/services/data-type/data-type.service';
 import { DataType } from 'src/app/services/database/models/data-type.model';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-content-type-form',
@@ -24,7 +25,10 @@ export class ContentTypeFormComponent implements OnInit, OnDestroy {
   public editorOptions: any;
 
   private whenNavigated$: ReplaySubject<string>;
-  private whenDestroyed$: Subject<any>;
+  private whenDestroyed$: Subject<void>;
+  private whenFormResetted$: Subject<void>;
+
+  private propertyAliasChanged$: Subject<string>;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,6 +37,7 @@ export class ContentTypeFormComponent implements OnInit, OnDestroy {
     private dataType: DataTypeService) {
     this.whenNavigated$ = new ReplaySubject<any>();
     this.whenDestroyed$ = new Subject<any>();
+    this.whenFormResetted$ = new Subject<any>();
 
     this.form = this.formBuilder.group({
       alias: [null],
@@ -49,6 +54,11 @@ export class ContentTypeFormComponent implements OnInit, OnDestroy {
     this.dataTypes$ = this.dataType.getAll().pipe(
       shareReplay(1)
     );
+    this.propertyAliasChanged$ = new Subject<string>();
+    this.propertyAliasChanged$.pipe(
+      pairwise(),
+      takeUntil(this.whenDestroyed$),
+    ).subscribe();
   }
 
   ngOnInit(): void {
@@ -57,25 +67,7 @@ export class ContentTypeFormComponent implements OnInit, OnDestroy {
     this.whenNavigated$.pipe(
       switchMap(alias => this.contentType.getOne(alias)),
       tap(contentType => this.original = contentType),
-      tap(contentType => {
-        this.form.reset();
-        this.form.patchValue({
-          alias: contentType.alias,
-          parentAlias: contentType.parentAlias,
-          template: contentType.template,
-        });
-        while (this.propertiesControls.length > 0) {
-          this.propertiesControls.pop();
-        }
-
-        for (const prop of contentType.properties) {
-          this.addProperty();
-          this.propertiesControls[this.propertiesControls.length - 1].patchValue({
-            alias: prop.alias,
-            dataTypeAlias: prop.dataTypeAlias,
-          });
-        }
-      }),
+      tap(contentType => this.revertForm()),
       takeUntil(this.whenDestroyed$)
     ).subscribe();
   }
@@ -88,11 +80,21 @@ export class ContentTypeFormComponent implements OnInit, OnDestroy {
     return (this.form.get('properties') as FormArray).controls;
   }
 
-  public newPropertyFormGroup(): FormGroup {
-    return this.formBuilder.group({
+  private newPropertyFormGroup(): FormGroup {
+    const formGroup = this.formBuilder.group({
       alias: [null],
       dataTypeAlias: [null],
     });
+
+    formGroup.get('alias').valueChanges.pipe(
+      pairwise(),
+      filter(changes => !!this.form.get('template').value),
+      tap(changes => this.form.patchValue({
+        template: this.form.get('template').value.replace(`{{${changes[0]}}}`, `{{${changes[1]}}}`),
+      })),
+    ).subscribe();
+
+    return formGroup;
   }
 
   public addProperty(): void {
@@ -101,6 +103,28 @@ export class ContentTypeFormComponent implements OnInit, OnDestroy {
 
   public removeProperty(idx: number): void {
     (this.form.get('properties') as FormArray).removeAt(idx);
+  }
+
+  public revertForm(): void {
+    const contentType = this.original;
+    this.form.reset();
+    this.whenFormResetted$.next();
+    this.form.patchValue({
+      alias: contentType.alias,
+      parentAlias: contentType.parentAlias,
+      template: contentType.template,
+    });
+    while (this.propertiesControls.length > 0) {
+      this.propertiesControls.pop();
+    }
+
+    for (const prop of contentType.properties) {
+      this.addProperty();
+      this.propertiesControls[this.propertiesControls.length - 1].patchValue({
+        alias: prop.alias,
+        dataTypeAlias: prop.dataTypeAlias,
+      });
+    }
   }
 
 }
