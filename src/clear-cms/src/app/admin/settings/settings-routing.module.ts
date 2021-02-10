080b310e9914897099570fa7bba5d6042216458d
Paul Coronia <qcoronia@gmail.com>
2021-02-11 00:01:10 +0800

    Add route to Data Type page

--- src/clear-cms/src/app/admin/settings/settings-routing.module.ts
+++ src/clear-cms/src/app/admin/settings/settings-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SettingsComponent } from './settings.component';
-import { ContentTypeExplorerComponent } from 'src/app/core/content-type-explorer/content-type-explorer.component';
 import { DatabaseManagementComponent } from 'src/app/core/database-management/database-management.component';
 
 const routes: Routes = [
   { path: '', component: SettingsComponent, children: [
     { path: 'content-types', loadChildren: () => import('src/app/core/content-type-explorer/content-type-explorer.module')
       .then(m => m.ContentTypeExplorerModule) },
+      { path: 'data-types', loadChildren: () => import('src/app/core/data-type-explorer/data-type-explorer.module')
+        .then(m => m.DataTypeExplorerModule) },
     { path: 'templates', loadChildren: () => import('src/app/core/template-explorer/template-explorer.module')
       .then(m => m.TemplateExplorerModule) },
     { path: 'database', component: DatabaseManagementComponent },
